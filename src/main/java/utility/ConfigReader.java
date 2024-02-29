ea35f31f0ec16770de4ebebf514d0bdf1297d6e1
afsan123 <afsanchowdhury5@gmail.com>
2024-03-01 03:43:44 +0600

    Initial commit

---
+++ src/main/java/utility/ConfigReader.java
@@ -0,0 +1,22 @@
+package utility;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ConfigReader {
+
+    private static final String CONFIG_FILE_PATH = "config.json";
+
+    public static String getAppUrl() {
+        try (FileReader fileReader = new FileReader(CONFIG_FILE_PATH)) {
+            JsonObject jsonObject = JsonParser.parseReader(fileReader).getAsJsonObject();
+            return jsonObject.get("appUrl").getAsString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
