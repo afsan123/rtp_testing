4dae197781897f024741d5dce73846662d53adcc
afsan123 <afsanchowdhury5@gmail.com>
2024-03-01 04:51:46 +0600

    login page completed

--- src/main/java/tests/CompanyDescriptionTest.java
+++ src/main/java/tests/CompanyDescriptionTest.java
@@ -18,5 +18,6 @@ public class CompanyDescriptionTest extends BaseClass {
         loginPage.enterEmail(email);
         loginPage.enterPassword(password);
         loginPage.clickOnLoginButton();
+
     }
 }
