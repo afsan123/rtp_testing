f032bbf5328877b106479129603905993987738b
afsan123 <afsanchowdhury5@gmail.com>
2024-03-06 01:07:29 +0600

    Brand Information Page Added

--- src/main/java/pageobjects/PromptTestPage.java
+++ src/main/java/pageobjects/PromptTestPage.java
@@ -1,17 +1,12 @@
 package pageobjects;
 
 import aquality.selenium.elements.interfaces.IButton;
-import aquality.selenium.elements.interfaces.ILabel;
-import aquality.selenium.elements.interfaces.ITextBox;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
 
 public class PromptTestPage extends Form {
     private final IButton socialBrandsBtn = getElementFactory().getButton(By.xpath("//span[text()='Social Brands Setup']"), "Social Brand Setup Button");
     private final IButton brandInformationBtn = getElementFactory().getButton(By.xpath("//button[contains(., 'Brand Information')]"), "Brand Information Button");
-    private final ITextBox brandName = getElementFactory().getTextBox(By.xpath("//input[@id='brand_name']"), "Brand Name");
-    private final ITextBox promptBrandName = getElementFactory().getTextBox(By.xpath("//p[contains(., 'brand name')]/strong"), "Prompt Brand Name");
-    private final ILabel versionList = getElementFactory().getLabel(By.xpath("(//div[@class='MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 css-1wk30yl']//span)[last()]"), "Version List");
 
     public PromptTestPage() {
         super(By.xpath("//span[text()='Social Brands Setup']"), " Prompt Test Page Unique Element");
